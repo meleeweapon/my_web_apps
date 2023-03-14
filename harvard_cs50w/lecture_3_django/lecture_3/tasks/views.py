a3c1f55c04d0ab84afda926c96e37ce8d2be4927
meleeweapon <mstar1907777@hotmail.com>
2023-03-14 21:01:41 +0300

    finished cs50w lecture 3, initiated django tutorial (polls)

--- harvard_cs50w/lecture_3_django/lecture_3/tasks/views.py
+++ harvard_cs50w/lecture_3_django/lecture_3/tasks/views.py
@@ -1,17 +1,32 @@
+from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
+class NewTaskForm(forms.Form):
+  task = forms.CharField(label="New Task")
+
 def index(request):
-  all_tasks = [
-    "go to market",
-    "go to home",
-    "go to kitchen",
-  ]
+  if "all_tasks" not in request.session:
+    request.session["all_tasks"] = []
   return render(request, "tasks/index.html", {
-    "tasklist": all_tasks,
+    "tasklist": request.session["all_tasks"],
   })
 
 def add(request):
+  if request.method == "POST":
+    form = NewTaskForm(request.POST)
+    if form.is_valid():
+      task = form.cleaned_data["task"]
+      request.session["all_tasks"] += [task]
+      return HttpResponseRedirect(reverse("tasks:index"))
+    else:
+      return render(request, "tasks/add.html", {
+        "form": form
+      })
+
   return render(request, "tasks/add.html", {
+    "form": NewTaskForm(),
   })
