58d87a9bacf3602974545378fe47fc4e5bc4055f
meleeweapon <mstar1907777@hotmail.com>
2023-03-24 20:15:13 +0300

    starting out, boiler-plate for model-view-presenter model

--- game2/game2.js
+++ game2/game2.js
@@ -1,9 +1,48 @@
 const canvasWidth = 1300
 const canvasHeight = 600
+const rootNode = document.querySelector("body")
 
-const canvas_node = document.createElement("canvas")
-canvas_node.setAttribute("id", "canvas")
-canvas_node.setAttribute("width", canvasWidth.toString())
-canvas_node.setAttribute("height", canvasHeight.toString())
-document.querySelector("body").appendChild(canvas_node)
+class Model {
+  constructor() {
 
+  }
+}
+
+class View {
+  constructor(canvasWidth, canvasHeight, rootNode) {
+    this.canvasWidth = canvasWidth
+    this.canvasHeight = canvasHeight
+    this.rootNode = rootNode
+    this.canvasNode = this.appendCanvas(this.rootNode)
+    this.context = this.canvasNode.getContext("2d")
+  }
+
+  appendCanvas(rootNode) {
+    const canvasNode = this.createCanvas(this.canvasWidth, this.canvasHeight)
+    rootNode.appendChild(canvasNode)
+    return canvasNode
+  }
+
+  createCanvas(width, height) {
+    const canvasNode = document.createElement("canvas")
+    canvasNode.setAttribute("id", "canvas")
+    canvasNode.setAttribute("width", width.toString())
+    canvasNode.setAttribute("height", height.toString())
+    return canvasNode
+  }
+}
+
+class Presenter {
+  constructor(model, view) {
+    this.model = model
+    this.view = view
+  }
+}
+
+
+const model = new Model()
+const view = new View(canvasWidth, canvasHeight, rootNode)
+const presenter = new Presenter(model, view)
+
+presenter.view.context.fillStyle = "white"
+presenter.view.context.fillRect(200, 200, 200, 200)
