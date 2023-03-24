12558d23da8170e2166ecc119003be758c7cab47
meleeweapon <mstar1907777@hotmail.com>
2023-03-24 18:09:22 +0300

    added base for game2

---
+++ game2/game2.js
@@ -0,0 +1,9 @@
+const canvasWidth = 1300
+const canvasHeight = 600
+
+const canvas_node = document.createElement("canvas")
+canvas_node.setAttribute("id", "canvas")
+canvas_node.setAttribute("width", canvasWidth.toString())
+canvas_node.setAttribute("height", canvasHeight.toString())
+document.querySelector("body").appendChild(canvas_node)
+
