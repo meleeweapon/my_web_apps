d863bbf60fda0db2fe6e00ad31f87751a280275b
meleeweapon <mstar1907777@hotmail.com>
2023-04-07 23:24:24 +0300

    harvard project 1 add entry delete entry

--- harvard_cs50w/projects/project_1/wiking/wiking_app/urls.py
+++ harvard_cs50w/projects/project_1/wiking/wiking_app/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
   path("", views.index, name="index"),
+  path("new_entry", views.new_entry, name="new_entry"),
   path("no_such_entry/", views.no_such_entry, name="no_such_entry"),
+  path("entry_added/", views.entry_added, name="entry_added"),
+  path("entry_add_failed/", views.entry_add_failed, name="entry_add_failed"),
   path("<entry_title>/", views.entry, name="entry"),
 ]
--- harvard_cs50w/projects/project_1/wiking/wiking_app/views.py
+++ harvard_cs50w/projects/project_1/wiking/wiking_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Entry
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 
 # Create your views here.
 def index(request):
-  return render(request, "wiking_app/index.html", {})
+  all_entries = Entry.objects.all()
+
+  return render(request, "wiking_app/index.html", {
+    "all_entries": all_entries,
+  })
 
 def entry(request, entry_title):
   try:
@@ -13,11 +17,44 @@ def entry(request, entry_title):
   except Entry.DoesNotExist:
     return HttpResponseRedirect(reverse("no_such_entry"))
 
+  if request.method == "POST":
+    if "delete_entry" in request.POST:
+      entry.delete()
+      return HttpResponseRedirect(reverse("index"))
+      
+
+
   return render(request, "wiking_app/entry.html", {
     "entry": entry,
+  })
+
+def new_entry(request):
+  if request.method == "POST":
+    title = request.POST["title"]
+    try:
+      Entry.objects.get(title=title)
+    except Entry.DoesNotExist:
+      exists = False
+    else:
+      exists = True
+
+    if not exists:
+      content = request.POST["content"]
+      Entry(title=title, content=content).save()
+      return HttpResponseRedirect(reverse("entry_added"))
+    else:
+      return HttpResponseRedirect(reverse("entry_add_failed"))
+
+  return render(request, "wiking_app/new_entry.html", {
   })
 
 def no_such_entry(request):
   return render(request, "wiking_app/no_such_entry.html", {
 
   })
+
+def entry_added(request):
+  return render(request, "wiking_app/entry_added.html")
+
+def entry_add_failed(request):
+  return render(request, "wiking_app/entry_add_failed.html")
