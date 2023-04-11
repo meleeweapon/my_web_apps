2ca8954f8ea030c6017f8501e9ad1aa31181d371
meleeweapon <mstar1907777@hotmail.com>
2023-04-11 20:11:33 +0300

    project 1 functionally finished

--- harvard_cs50w/projects/project_1/wiking/wiking_app/urls.py
+++ harvard_cs50w/projects/project_1/wiking/wiking_app/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
   path("", views.index, name="index"),
+  path("search", views.search, name="search"),
   path("new_entry", views.new_entry, name="new_entry"),
   path("no_such_entry/", views.no_such_entry, name="no_such_entry"),
   path("entry_added/", views.entry_added, name="entry_added"),
+  path("random_redirect/", views.random_redirect, name="random_redirect"),
   path("entry_add_failed/", views.entry_add_failed, name="entry_add_failed"),
   path("<entry_title>/", views.entry, name="entry"),
+  path("<entry_title>/edit_entry/", views.edit_entry, name="edit_entry"),
 ]
--- harvard_cs50w/projects/project_1/wiking/wiking_app/views.py
+++ harvard_cs50w/projects/project_1/wiking/wiking_app/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from .models import Entry
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from random import choice
+from markdown2 import Markdown
+
+md = Markdown()
+
+def get_entry(entry_title):
+  try:
+    entry = Entry.objects.get(title=entry_title)
+  except Entry.DoesNotExist:
+    entry = None
+  return entry
+
+def entry_url(entry_title):
+  return f'{reverse("index")}{entry_title}'
+
 
 # Create your views here.
 def index(request):
@@ -12,20 +27,21 @@ def index(request):
   })
 
 def entry(request, entry_title):
-  try:
-    entry = Entry.objects.get(title=entry_title)
-  except Entry.DoesNotExist:
+  entry = get_entry(entry_title)
+  
+  if not entry:
     return HttpResponseRedirect(reverse("no_such_entry"))
 
   if request.method == "POST":
     if "delete_entry" in request.POST:
       entry.delete()
       return HttpResponseRedirect(reverse("index"))
-      
 
 
+  entry_content = md.convert(entry.content)
   return render(request, "wiking_app/entry.html", {
-    "entry": entry,
+    "entry_title": entry.title,
+    "entry_content": entry_content,
   })
 
 def new_entry(request):
@@ -41,7 +57,8 @@ def new_entry(request):
     if not exists:
       content = request.POST["content"]
       Entry(title=title, content=content).save()
-      return HttpResponseRedirect(reverse("entry_added"))
+      return HttpResponseRedirect(entry_url(title))
+      # return HttpResponseRedirect(reverse("entry_added"))
     else:
       return HttpResponseRedirect(reverse("entry_add_failed"))
 
@@ -58,3 +75,35 @@ def entry_added(request):
 
 def entry_add_failed(request):
   return render(request, "wiking_app/entry_add_failed.html")
+
+def random_redirect(request):
+  random_entry = choice(Entry.objects.all())
+
+  return HttpResponseRedirect(entry_url(random_entry.title))
+
+def edit_entry(request, entry_title):
+  entry = get_entry(entry_title)
+
+  if request.method == "POST":
+    entry.content = request.POST["content"]
+    entry.save()
+    return HttpResponseRedirect(entry_url(entry.title))
+
+
+  return render(request, "wiking_app/edit_entry.html", {
+    "entry_title": entry.title,
+    "entry_content": entry.content,
+  })
+
+
+def search(request):
+  search_text = request.GET["title"]
+  entry = get_entry(search_text)
+
+  if entry:
+    return HttpResponseRedirect(entry_url(entry.title))
+  
+  search_results = Entry.objects.filter(title__contains=search_text)
+  return render(request, "wiking_app/search.html", {
+    "search_results": search_results,
+  })
