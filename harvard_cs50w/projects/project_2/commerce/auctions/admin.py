df60c5900f43b30aaf2489cddc932c1de9df8455
meleeweapon <mstar1907777@hotmail.com>
2023-04-21 21:21:55 +0300

    makin some models, trying stuff

--- harvard_cs50w/projects/project_2/commerce/auctions/admin.py
+++ harvard_cs50w/projects/project_2/commerce/auctions/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Bid, Comment, Auction
+
+class AdminAuction(admin.ModelAdmin):
+  list_display = ("id", "title", "description", "holder")
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Auction, AdminAuction)
--- harvard_cs50w/projects/project_2/commerce/auctions/models.py
+++ harvard_cs50w/projects/project_2/commerce/auctions/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+ms = models
 
 class User(AbstractUser):
-    pass
+  pass
+
+class Auction(ms.Model):
+  title = ms.CharField(max_length=64)
+  description = ms.CharField(max_length=2048)
+
+#   bids = ms.ForeignKey(
+#     Bid, on_delete=ms.CASCADE, related_name="auction_bids"
+#   )
+#   comments = ms.ForeignKey(
+#     Comment, on_delete=ms.CASCADE, related_name="auction_comments"
+#   )
+
+  holder = ms.ForeignKey(
+    User, on_delete=ms.CASCADE, related_name="user_auctions"
+  )
+
+class Comment(ms.Model):
+  content = ms.CharField(max_length=1024)
+  comment_auction = ms.ForeignKey(
+    Auction, on_delete=ms.CASCADE, related_name="auction_comments"
+  )
+
+class Bid(ms.Model):
+  amount = ms.FloatField()
+  bid_auction = ms.ForeignKey(
+    Auction, on_delete=ms.CASCADE, related_name="auction_bids"
+  )
