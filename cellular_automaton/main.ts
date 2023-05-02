2c39b267bbc82a63ebf5a3317828c62aee951c70
meleeweapon <mstar1907777@hotmail.com>
2023-05-02 21:41:44 +0300

    conting automaton

--- cellular_automaton/main.ts
+++ cellular_automaton/main.ts
@@ -1,78 +1,84 @@
-// current_cell = 'dead' | 'alive';
-// neighbour_count = ...; // 0...8
-// next_cell = undefined;
-// switch (current_cell) {
-//   case 'dead': {
+const S = 2;
+const GoL = {
+  'dead': {
+    '3': 'alive',
+    'else': 'dead',
+  },
+  'alive': {
+    '2': 'alive',
+    '3': 'alive',
+    'else': 'dead',
+  }
+}
 
-//   } break;
-
-//   case 'alive': {
-
-//   } break;
-// }
+current_cell = 'dead' | 'alive';
+neighbour_count = ...; // 0...8
+assert(0 <= neighbour_count %% neighbour_count <= 8)
+next_cell = undefined;
+next_cell = GoL[current_cell][neighbour_count]
 // _-=======================================================-_
 
-function cell_step(cell_state, neighbour_count) {
-  if (cell_state == "alive") {
-    if (neighbour_count < 2) return 'dead';
-    if (neighbour_count < 4) return 'alive';
-    return 'dead';
-  } else {
-    if (neighbour_count == 3) return 'alive';
-  }
-}
+// function cell_step(cell_state, neighbour_count) {
+//   if (cell_state == "alive") {
+//     if (neighbour_count < 2) return 'dead';
+//     if (neighbour_count < 4) return 'alive';
+//     return 'dead';
+//   } else {
+//     if (neighbour_count == 3) return 'alive';
+//   }
+// }
 
-function count_neighbours(map, x, y) {
-  let count = 0
-  let neighbours = [
-    map[x-1][y+1], map[x][y+1], map[x+1][y+1],
-    map[x-1][y]               , map[x+1][y],
-    map[x-1][y-1], map[x][y-1], map[x+1][y-1]
-  ]
-  console.log(neighbours)
-  neighbours.forEach(cell => {
-    if (cell == 'alive') count += 1;
-  });
+// function count_neighbours(map, x, y) {
+//   let count = 0
+//   let neighbours = [
+//     map[x-1][y+1], map[x][y+1], map[x+1][y+1],
+//     map[x-1][y]               , map[x+1][y],
+//     map[x-1][y-1], map[x][y-1], map[x+1][y-1]
+//   ]
+//   console.log(neighbours)
+//   neighbours.forEach(cell => {
+//     if (cell == 'alive') count += 1;
+//   });
   
-  return count;
-}
+//   return count;
+// }
 
-function render_map(canvas, canvas_context, map) {
-  canvas_context.fillStyle = 'white'
-  canvas_context.fillRect(0, 0, canvas.width, canvas.height)
-  canvas_context.fillStyle = 'blue'
-  let cols = map.length
-  let rows = map[0].length
-  let box_size_x = canvas.width / cols
-  let box_size_y = canvas.height / rows
+// function render_map(canvas, canvas_context, map) {
+//   canvas_context.fillStyle = 'white'
+//   canvas_context.fillRect(0, 0, canvas.width, canvas.height)
+//   canvas_context.fillStyle = 'blue'
+//   let cols = map.length
+//   let rows = map[0].length
+//   let box_size_x = canvas.width / cols
+//   let box_size_y = canvas.height / rows
 
-  for (let x = 0; x < cols; x += 1) {
-    for (let y = 0; y < rows; y += 1) {
-      if (map[x][y] == 'alive') {
-        canvas_context.fillRect(
-          box_size_x * x,
-          box_size_y * y,
-          box_size_x,
-          box_size_y
-        )
-      }
-    }
-  }
-}
-
-
-let the_map = [
-  ['alive', 'dead', 'alive'],
-  ['dead', 'alive', 'dead'],
-  ['alive', 'dead', 'alive']
-];
+//   for (let x = 0; x < cols; x += 1) {
+//     for (let y = 0; y < rows; y += 1) {
+//       if (map[x][y] == 'alive') {
+//         canvas_context.fillRect(
+//           box_size_x * x,
+//           box_size_y * y,
+//           box_size_x,
+//           box_size_y
+//         )
+//       }
+//     }
+//   }
+// }
 
 
-let c = document.querySelector('#the-canvas')
-let ctx = c.getContext('2d')
-ctx.fillStyle = 'blue'
+// let the_map = [
+//   ['alive', 'dead', 'alive'],
+//   ['dead', 'alive', 'dead'],
+//   ['alive', 'dead', 'alive']
+// ];
 
-render_map(c, ctx, the_map)
+
+// let c = document.querySelector('#the-canvas')
+// let ctx = c.getContext('2d')
+// ctx.fillStyle = 'blue'
+
+// render_map(c, ctx, the_map)
 
 
-// export {};
+// // export {};
