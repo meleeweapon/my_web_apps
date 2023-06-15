c319a6938d85b8740dd351a2333b9f7cd8c939de
meleeweapon <mstar1907777@hotmail.com>
2023-06-15 15:43:18 +0300

    task management looking bad, needs refactor.

--- task_management/task_management.js
+++ task_management/task_management.js
@@ -10,25 +10,58 @@ class Task {
 }
 class TaskDatabase {
     constructor() {
-        this.tasks = [];
+        this.tasks = [new Task("jfdkslall", "jfkdlsaş", new Date(), null)];
     }
     add_task(task) {
         this.tasks.push(task);
     }
+    get_tasks() {
+        return this.tasks;
+    }
 }
 const taskDataBase = new TaskDatabase();
 const viewModel = {
+    // make getter setters
     title: "",
     description: "",
+    tasksToDisplay: [],
     addTask() {
         const task = new Task(this.title, this.description, new Date(), null);
         taskDataBase.add_task(task);
+    },
+    fetchTasks() {
+        this.tasksToDisplay = taskDataBase.get_tasks();
+    },
+    renderTasks(taskListElemnt) {
+        while (taskListElemnt.firstChild) {
+            taskListElemnt.firstChild.remove();
+        }
+        for (const task of viewModel.tasksToDisplay) {
+            const taskElement = createTaskElement(task);
+            taskListElemnt.appendChild(taskElement);
+            console.log(task);
+        }
     }
 };
+const createTaskElement = (task) => {
+    const taskElement = document.createElement("div");
+    taskElement.setAttribute("class", "task");
+    const taskTitleElement = document.createElement("div");
+    taskTitleElement.setAttribute("class", "task-title");
+    taskTitleElement.textContent = task.title;
+    const taskDescriptionElement = document.createElement("div");
+    taskDescriptionElement.setAttribute("class", "task-description");
+    taskDescriptionElement.textContent = task.description;
+    taskElement.appendChild(taskTitleElement);
+    taskElement.appendChild(taskDescriptionElement);
+    return taskElement;
+};
 const bindData = () => {
+    // add task block
     const titleInputElement = document.querySelector("#title");
     const descriptionInputElement = document.querySelector("#description");
     const addTaskButton = document.querySelector("#add-task");
+    const taskListElement = document.querySelector(".task-list");
     titleInputElement.addEventListener("input", (event) => {
         viewModel.title = titleInputElement.value;
     });
@@ -37,6 +70,12 @@ const bindData = () => {
     });
     addTaskButton.addEventListener("click", (event) => {
         viewModel.addTask();
+        viewModel.fetchTasks();
+        viewModel.renderTasks(taskListElement);
+    });
+    taskListElement.addEventListener("", (event) => {
+        viewModel.fetchTasks();
+        viewModel.renderTasks(taskListElement);
     });
 };
 document.addEventListener("DOMContentLoaded", (event) => {
--- task_management/task_management.ts
+++ task_management/task_management.ts
@@ -12,11 +12,15 @@ class TaskDatabase {
   public tasks: Task[];
 
   constructor() {
-    this.tasks = [];
+    this.tasks = [new Task("jfdkslall", "jfkdlsaş", new Date(), null)];
   }
 
-  add_task(task: Task) {
+  add_task(task: Task): void {
     this.tasks.push(task);
+  }
+
+  get_tasks(): Task[] {
+    return this.tasks;
   }
 }
 
@@ -27,22 +31,61 @@ interface ViewModel {
   title: string;
   description: string;
   // deadline: Date;
+  tasksToDisplay: Task[];
   addTask(): void;
+  fetchTasks(): void;
+  renderTasks(taskListElemnt: HTMLElement): void;
 }
 
 const viewModel: ViewModel = {
+  // make getter setters
   title: "",
   description: "",
+  tasksToDisplay: [],
   addTask(): void {
     const task = new Task(this.title, this.description, new Date(), null);
     taskDataBase.add_task(task);
+  },
+  fetchTasks(): void {
+    this.tasksToDisplay = taskDataBase.get_tasks();
+  },
+  renderTasks(taskListElemnt: HTMLElement): void {
+    while (taskListElemnt.firstChild) {
+      taskListElemnt.firstChild.remove()
+    }
+    for (const task of viewModel.tasksToDisplay) {
+      const taskElement = createTaskElement(task);
+      taskListElemnt.appendChild(taskElement);
+      console.log(task)
+    }
   }
 }
 
+const createTaskElement = (task: Task): HTMLElement => {
+  const taskElement = document.createElement("div");
+  taskElement.setAttribute("class", "task");
+
+  const taskTitleElement = document.createElement("div");
+  taskTitleElement.setAttribute("class", "task-title");
+  taskTitleElement.textContent = task.title;
+
+  const taskDescriptionElement = document.createElement("div");
+  taskDescriptionElement.setAttribute("class", "task-description");
+  taskDescriptionElement.textContent = task.description;
+
+  taskElement.appendChild(taskTitleElement);
+  taskElement.appendChild(taskDescriptionElement);
+
+  return taskElement;
+}
+
 const bindData = () => {
+  // add task block
   const titleInputElement = document.querySelector("#title") as HTMLInputElement;
   const descriptionInputElement = document.querySelector("#description") as HTMLInputElement;
   const addTaskButton = document.querySelector("#add-task") as HTMLButtonElement;
+
+  const taskListElement = document.querySelector(".task-list") as HTMLUListElement;
 
   titleInputElement.addEventListener("input", (event) => {
     viewModel.title = titleInputElement.value;
@@ -54,6 +97,14 @@ const bindData = () => {
 
   addTaskButton.addEventListener("click", (event) => {
     viewModel.addTask();
+
+    viewModel.fetchTasks();
+    viewModel.renderTasks(taskListElement);
+  })
+
+  taskListElement.addEventListener("", (event) => {
+    viewModel.fetchTasks();
+    viewModel.renderTasks(taskListElement);
   })
 }
 
