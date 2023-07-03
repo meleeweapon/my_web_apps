a02852b7c604f56fae87f6662abbaa729e1a4568
meleeweapon <mstar1907777@hotmail.com>
2023-07-03 15:14:53 +0300

    tryna solve quiz model, i might have overcompicated things, don't forget to ask chatgpt's opinion

--- quizzyInTS/quiz.ts
+++ quizzyInTS/quiz.ts
@@ -15,22 +15,42 @@ class Id {
   }
 }
 
+type Choice = string;
+
 class Choices {
   constructor(
-    public a: string,
-    public b: string,
-    public c: string,
-    public d: string
+    private choices: Choice[],
   ) {
 
   }
-  isValidChoice(choice: string) {
-    return choice in Object.values(this);
+  isValidChoice(choice: Choice) {
+    return choice in this.choices;
+  }
+  getAll(): string[] {
+    return [...this.choices];
+  }
+}
+
+class Answer {
+  constructor(
+    public questionId: Id,
+    public choice: Choice,
+  ) {
+  }
+}
+
+class Player {
+  public answers: Answer[];
+  constructor(
+    public name: string,
+    answers?: Answer[],
+  ) {
+    this.answers = !answers ? [] : answers;
   }
 }
 
 class Question {
-  public answer: string,
+  public correctAnswer: string;
   constructor(
     public id: Id,
     public question: string,
@@ -40,18 +60,71 @@ class Question {
     if (!choices.isValidChoice(correctAnswer)) {
       throw new Error("invalid answer");
     }
+    this.correctAnswer = correctAnswer;
   }
 }
 
-class QuizRound {
-  public choice: string | null;
+// class QuizRound {
+//   constructor(
+//     public question: Question,
+//   ) {
+//   }
+// }
+
+type Result = "correct" | "wrong" | "empty";
+
+class AnswerResult {
   constructor(
-    public question: Question,
+    public answer: Answer,
+    public result: Result,
   ) {
-    this.choice = null;
-  }
-
-  makeChoice(string): void {
-    this.choice = string;
   }
 }
+
+class PlayerResult {
+  constructor(
+    public player: Player,
+    public score: number,
+    public answersCorrect: AnswerResult[],
+  ) {
+  }
+}
+
+class QuizGame {
+  constructor(
+    public players: Player[],
+    public questions: Question[],
+  ) {}
+  public scores(): PlayerResult[] {
+    const playerResults = this.players.map(player => {
+      const score = 0;
+      // const answersCorrect = player.answers
+      //   .map(answer => {
+      //     if (answer. in this.questions)
+      //     const result = 
+      //     new AnswerResult(answer, );
+      //   })
+      const answerResults = this.questions.map(question => {
+        let result: Result;
+        if (question.id.value in player.answers.map(a => a.questionId.value)) {
+          const answer = player.answers.filter(a => a.questionId === question.id)[0];
+          if (
+            question.correctAnswer 
+            === 
+            answer.choice
+          ) {
+            result = "correct";
+          } else {
+            result = "wrong";
+          }
+        } else { result = "empty" }
+        new AnswerResult(new Answer(answer), result);
+      });
+      const playerResult = new PlayerResult(player, score, answerResults);
+      return playerResult;
+    });
+    return playerResults;
+  }
+}
+
+const player = new Player("awooga");
